b79d7e0cf07187ad4682653826eacfe86599b9ea
I-Hudson <cuffy94@gmail.com>
2023-01-07 18:04:26 +0000

    Suppress warning.

--- Splash/inc/SplashScreen.h
+++ Splash/inc/SplashScreen.h
@@ -6,7 +6,9 @@
 #elif SPLASH_PLATFORM_UNIX
 	using SplashScreen = SplashScreenUnix;
 #else 
+#ifndef SPLASH_PLATFORM_NO
 #pragma message ( "WARNING: [Splash] No platform define has been defined." ) 
+#endif
 #include "Splash/Platforms/SplashScreenEmpty.h"
 	using SplashScreen = Splash::SplashScreenEmpty;
 #endif
